5c83d99209164e3c64f6dd99c36d34c64a410553
michaelhsladetrng <51245469+michaelhsladetrng@users.noreply.github.com>
2019-12-18 19:36:45 +0000

    before PM break

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('few400-testing app is running!');
+    expect(page.getTitleText()).toEqual('Testing is Fun');
   });
 
   afterEach(async () => {
---
+++ e2e/src/books/books.e2e-spec.ts
@@ -0,0 +1,15 @@
+import { BooksComponentPage } from './books.po';
+
+describe('the books componet', () => {
+  let page: BooksComponentPage;
+
+  beforeEach(() => {
+    page = new BooksComponentPage();
+  });
+  it('can load the list', () => {
+    page.navigateTo();
+    page.clickTheLoadButton();
+
+    expect(page.getFirstItemOnTheList()).toBe('War of the World by Wells');
+  });
+});
